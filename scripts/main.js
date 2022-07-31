918342385155bc9e633e38bea67e22c858d54af5
yuyu <yuyutw123@gmail.com>
2022-07-31 19:05:54 +0800

    message

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+let userInput = document.getElementById("input-bar");
+let submitBtn = document.querySelector(".btn-input");
+const token = null
+
+
+async function enterValue(value) {
+  let str = {
+    "longUrl": userInput.value
+  };
+  let headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "Authorization": `Bearer ${token}`,
+}
+  console.log(str.longUrl);
+
+  fetch ("http://127.0.0.1:5000/api/url/shorten", {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(str)
+  }).then(function(response){
+    return response.json();
+  }).then(function(response){
+    let result = document.getElementById('output').value = response.shortUrl;
+  })
+  .catch(function(error){
+    console.log('Error during fetch: ' + error.message);
+  });
+
+}
+
+function copyInput() {
+  var copyText = document.getElementById("output");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  alert("已複製" + copyText.value);
+}
+
+submitBtn.addEventListener("click", enterValue);
